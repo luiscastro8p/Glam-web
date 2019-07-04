2ad34147d0a1a31b27989bba900035483a2a9c27
luiscastro8p <luisarmandocastro12@gmail.com>
2019-07-03 22:12:07 -0600

    [first commit]Se agregaron componentes forgot-password, login, navbar y perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
+import { LoginComponent } from './componentes/shared/login/login.component';
+import { RegistroComponent } from './componentes/shared/registro/registro.component';
+import { ForgotPasswordComponent } from './componentes/shared/forgot-password/forgot-password.component';
+import { PerfilComponent } from './componentes/shared/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: NavbarComponent },
+  { path:'login', component: LoginComponent },
+  { path:'registro', component: RegistroComponent },
+  { path:'olvidaste-contraseña', component: ForgotPasswordComponent },
+  { path:'perfil', component: PerfilComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//servicios
+//componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/shared/login/login.component';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
+import { RegistroComponent } from './componentes/shared/registro/registro.component';
+import { ForgotPasswordComponent } from './componentes/shared/forgot-password/forgot-password.component';
+import { PerfilComponent } from './componentes/shared/perfil/perfil.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegistroComponent,
+    ForgotPasswordComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
